6996f6b38c6f14eb18b387eeaa775a66630207b4
Abuzar-cpu <kerimovabuzer25@gmail.com>
2022-01-29 12:37:38 +0400

    giphs has fixed width

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $("#add-button").on("click", () =>{
     if($(".form-control").val() != '')
     {
-        let button = $("<button onclick='getGiph(this)' type='button' class='getGiph ms-3 mt-3 btn btn-primary btn-lg'>" + $(".form-control").val() + "</button>");
+        let button = $("<button onclick='getGiph(this)' type='button' class='getGiph ms-3 mt-3 btn btn-primary'>" + $(".form-control").val() + "</button>");
 
         $("#buttons").append(button);
         $(".form-control").val("");
@@ -16,19 +16,11 @@ let getGiph = (element) =>
     let responseData;
     let searchTerm = element.innerText.toLowerCase();
     fetch("https://api.giphy.com/v1/gifs/search?api_key=NA37wZ2PZFORU0z90xzFHt4xpYCtT5e6&q="+searchTerm+"&limit=5&offset=0&rating=g&lang=en").then(result => result.json()).then(response => {
-        
-    console.log("Sending request");
         responseData = response.data;
 
-        // let url = responseData[0].images;
-        // console.log(url)
-        console.log("we got the data", responseData);
         for(const giph of responseData)
         {
-            
-            // let still = giph.images.fixed_height_small_still.url;
-            let vid = giph.images.downsized_medium.url;
-            // console.log(url);
+            let vid = giph.images.fixed_width.url;
             $("#giphs").append("<img onclick='playPause(this)' data-playing='false' src='" + vid + "'/>");
         }
     }).catch("Error occured");
@@ -36,17 +28,13 @@ let getGiph = (element) =>
 
 let playPause = (element, still, vid) =>
 {
-    // console.log(still, vid);
     if($(element).data("playing") == "false")
     {
-        // $(element).attr("src", vid);
         $(element).data("playing","true");
-        console.log("It was false")
         return;
     }
 
     else{
-        // $(element).attr("src", still);
         $(element).data("playing", "false");
         return;
     }
